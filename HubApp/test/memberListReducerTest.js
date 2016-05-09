fa1c898d9757e9c77258600c5eb0e5eee1966f4d
Livio Bieri <livio@livio.li>
2016-05-09 10:06:54 +0200

    👻 Reducer done.

--- HubApp/test/memberListReducerTest.js
+++ HubApp/test/memberListReducerTest.js
@@ -1,7 +1,9 @@
 import expect from 'expect';
 import { memberList } from '../App/Reducers/memberListReducer';
+import { requestMemberList, receiveMemberList } from '../App/Actions/memberListActions';
 
 describe('Memberlist Reducer', () => {
+
   it('should return the initial state', () => {
     expect(
       memberList(undefined, {})
@@ -10,4 +12,36 @@ describe('Memberlist Reducer', () => {
       loading: true
     });
   });
+
+  it('should handle requestMemberList', () => {
+    expect(
+      memberList({
+        loading: false,
+        memberList: []
+      }, requestMemberList())
+    ).toEqual({
+      memberList: [],
+      loading: true
+    });
+  });
+
+
+  const expectedMemberList = [
+    { lastName: 'Brunner', firstName: 'Raphi' },
+    { lastName: 'Blatter', firstName: 'Sepp' },
+    { lastName: 'Bieri', firstName: 'Livio' }
+  ];
+
+  it('should handle receiveMemberList', () => {
+    expect(
+      memberList({
+        loading: true,
+        memberList: []
+      }, receiveMemberList(expectedMemberList))
+    ).toEqual({
+      memberList: expectedMemberList,
+      loading: false
+    });
+  });
+
 });
