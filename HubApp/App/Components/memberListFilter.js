3a92985d9cc759659f8c79189add18e4f9d1011c
Livio Bieri <livio@livio.li>
2016-05-12 23:14:20 +0200

    📌 Added second example filter

--- HubApp/App/Components/memberListFilter.js
+++ HubApp/App/Components/memberListFilter.js
@@ -13,7 +13,7 @@ import { color } from '../Styles/color';
 import { CoolButton } from '../Styles/button';
 import ListFilterItem from './listFilterItem';
 
-export const MemberListFilter = ({ onApplyFilter, ...props }) => {
+export const MemberListFilter = ({ onApplyFilter, filter, ...props }) => {
   return (
     <Animated.View style={styles.container}>
       <View style={{ flex: 1}}>
@@ -21,8 +21,14 @@ export const MemberListFilter = ({ onApplyFilter, ...props }) => {
           key={'JavaScript'}
           topic={'JavaScript'}
           color={color.red}
-          isChecked={false}
+          isChecked={filter.includes('JavaScript')}
           onToggle={() => {onApplyFilter(['JavaScript'])}} />
+        <ListFilterItem
+          key={'CSS'}
+          topic={'CSS'}
+          color={color.red}
+          isChecked={filter.includes('CSS')}
+          onToggle={() => {onApplyFilter(['CSS'])}} />
       </View>
       <CoolButton caption={'Apply Filters'} onPress={() => props.onNavigate({type: 'BackAction'})}/>
     </Animated.View>
