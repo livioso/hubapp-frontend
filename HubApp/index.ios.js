0ee0dfcd21ee21ec8cfda18acc2b0269572bbc30
Livio Bieri <livio@livio.li>
2016-06-06 09:58:40 +0200

    Removed unused code.

--- HubApp/index.ios.js
+++ HubApp/index.ios.js
@@ -1,7 +1,6 @@
 // import { Playground as HubApp } from './App/Playground';
 // import { Styleguide as HubApp } from './App/Styleguide';
 import { HubApp } from './App/HubApp';
-// import { ModifyTags as HubApp } from './App/Components/modifyTags';
 
 import {
   AppRegistry,
