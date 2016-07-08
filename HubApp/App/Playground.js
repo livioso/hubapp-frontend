7e36a393346e608a2458e825625f61009b25a802
Livio Bieri <livio@livio.li>
2016-07-08 15:51:33 +0200

    Playground: Maybe this could be an idea for start screen?

--- HubApp/App/Playground.js
+++ HubApp/App/Playground.js
@@ -1,41 +1,64 @@
 /* @flow weak */
 /* eslint-disable no-alert, no-console, max-len */
 import React from 'react';
-import ReactNative from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
-const {
+import {
+  Text,
   View,
   ScrollView,
-  StyleSheet
-} = ReactNative;
+  StyleSheet,
+  Dimensions
+} from 'react-native';
+
+import {color} from './Styles/color';
 
 export const Playground = () => {
-  const member = {
-    firstname: 'Livio',
-    lastname: 'Bieri',
-    email: 'livio@livio.li',
-    position: 'Leader at Evil Corp',
-    shortDescription: 'I build stuff for the web. Mainly React.',
-    skills: ['React', 'ReactNative', 'JavaScript', 'Lean UX'],
-    picture: 'https://avatars0.githubusercontent.com/u/1672198?v=3&s=460',
-  };
 
   return (
     <ScrollView>
-      <View style={styles.container}>
-        <Icon name="home" size={30} color="#900" />
+      <View style={{flexDirection: 'row', flexWrap: 'wrap', flex: 1}}>
+        <View style={[styles.tile, {backgroundColor: color.blue}]}>
+          <Icon name="public" size={30} color="white" />
+          <Text style={{ color: 'white', paddingTop: 3 }}>All Members</Text>
+        </View>
+        <View style={[styles.tile, {backgroundColor: color.red}]}>
+          <Icon name="map" size={30} color="white" />
+          <Text style={{ color: 'white', paddingTop: 3 }}>Currently Here</Text>
+        </View>
+        <View style={[styles.tile, {backgroundColor: color.green}]}>
+          <Icon name="today" size={30} color="white" />
+          <Text style={{ color: 'white', paddingTop: 3 }}>New Members</Text>
+        </View>
+        <View style={[styles.tile, {backgroundColor: color.orange}]}>
+          <Icon name="home" size={30} color="white" />
+          <Text style={{ color: 'white', paddingTop: 3 }}>Staff</Text>
+        </View>
+        <View style={[styles.row, {backgroundColor: color.blue}]}>
+          <Icon name="email" size={30} color="white" />
+          <Text style={{ color: 'white', paddingTop: 3 }}>Email Your Host</Text>
+        </View>
+        <View style={[styles.row, {backgroundColor: color.green}]}>
+          <Icon name="event" size={30} color="white" />
+          <Text style={{ color: 'white', paddingTop: 3 }}>Events</Text>
+        </View>
       </View>
     </ScrollView>
   );
+
 };
 
+const width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 60,
-    paddingBottom: 100,
+  tile: {
+    height: width / 2,
+    width: width / 2,
     alignItems: 'center',
-    backgroundColor: 'white'
+    justifyContent: 'center'
+  },
+  row: {
+    height: width / 4,
+    width: width,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
