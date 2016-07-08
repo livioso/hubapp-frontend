871c4c947aa2486e60bd2243f6b6f88b8f5c6341
Livio Bieri <livio@livio.li>
2016-07-08 16:30:40 +0200

    Cleaned up eslint errors in Playground.js

--- HubApp/App/Playground.js
+++ HubApp/App/Playground.js
@@ -10,41 +10,39 @@ import {
   Dimensions
 } from 'react-native';
 
-import {color} from './Styles/color';
+import { color } from './Styles/color';
 
 export const Playground = () => {
-
   return (
     <ScrollView>
-      <View style={{flexDirection: 'row', flexWrap: 'wrap', flex: 1}}>
-        <View style={[styles.tile, {backgroundColor: color.blue}]}>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap', flex: 1 }}>
+        <View style={[styles.tile, { backgroundColor: color.blue }]}>
           <Icon name="public" size={30} color="white" />
           <Text style={{ color: 'white', paddingTop: 3 }}>All Members</Text>
         </View>
-        <View style={[styles.tile, {backgroundColor: color.red}]}>
+        <View style={[styles.tile, { backgroundColor: color.red }]}>
           <Icon name="map" size={30} color="white" />
           <Text style={{ color: 'white', paddingTop: 3 }}>Currently Here</Text>
         </View>
-        <View style={[styles.tile, {backgroundColor: color.green}]}>
+        <View style={[styles.tile, { backgroundColor: color.green }]}>
           <Icon name="today" size={30} color="white" />
           <Text style={{ color: 'white', paddingTop: 3 }}>New Members</Text>
         </View>
-        <View style={[styles.tile, {backgroundColor: color.orange}]}>
+        <View style={[styles.tile, { backgroundColor: color.orange }]}>
           <Icon name="home" size={30} color="white" />
           <Text style={{ color: 'white', paddingTop: 3 }}>Staff</Text>
         </View>
-        <View style={[styles.row, {backgroundColor: color.blue}]}>
+        <View style={[styles.row, { backgroundColor: color.blue }]}>
           <Icon name="email" size={30} color="white" />
           <Text style={{ color: 'white', paddingTop: 3 }}>Email Your Host</Text>
         </View>
-        <View style={[styles.row, {backgroundColor: color.green}]}>
+        <View style={[styles.row, { backgroundColor: color.green }]}>
           <Icon name="event" size={30} color="white" />
           <Text style={{ color: 'white', paddingTop: 3 }}>Events</Text>
         </View>
       </View>
     </ScrollView>
   );
-
 };
 
 const width = Dimensions.get('window').width;
@@ -56,9 +54,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   row: {
-    height: width / 4,
-    width: width,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    height: width / 4,
+    width,
   },
 });
