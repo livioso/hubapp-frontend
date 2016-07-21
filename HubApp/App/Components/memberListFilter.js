b8278fc855f39f4eb47e9e4e6de3e50fed480e41
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-07-21 12:32:24 +0200

    eslint stuff fixed

--- HubApp/App/Components/memberListFilter.js
+++ HubApp/App/Components/memberListFilter.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import {
   Dimensions,
   ScrollView,
   StyleSheet,
   View,
   Image,
-  TouchableHighlight,
   TouchableOpacity,
   NavigationExperimental,
 } from 'react-native';
@@ -14,81 +13,81 @@ import { color } from '../Styles/color';
 import { Text, HeaderText } from '../Styles/text';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export const MemberListFilter = ({ onToggleFilter, activeFilters, membersCount, resetAll }) => {
+export const MemberListFilter = ({ onToggleFilter, activeFilters, membersCount, resetAll }) => { // eslint-disable-line complexity
   return (
     <ScrollView>
       <View style={{ flexDirection: 'row', flexWrap: 'wrap', flex: 1 }}>
-        <TouchableOpacity style={ [styles.tile, {width: width}] } activeOpacity={0.7} onPress={() => onToggleFilter('colab')}>
-          <Image resizeMode='cover' style={ [styles.tile, {width: width}] } source={require('../Styles/Assets/IHZ_150916_master_locations_3.jpg')} />
-          <View style={ [styles.wrap, { width: width }] }>
-            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('colab')? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)'}]}>
-              <Icon name='location-on' size={40} color='white' />
+        <TouchableOpacity style={ [styles.tile, { width }] } activeOpacity={0.7} onPress={() => onToggleFilter('colab')}>
+          <Image resizeMode="cover" style={ [styles.tile, { width }] } source={require('../Styles/Assets/IHZ_150916_master_locations_3.jpg')} />
+          <View style={ [styles.wrap, { width }] }>
+            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('colab') ? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)' }]}>
+              <Icon name="location-on" size={40} color="white" />
               <HeaderText style={{ color: color.light }}>Colab</HeaderText>
               <View style={{ flexDirection: 'row', paddingTop: 5 }}>
                 <Text style={{ paddingRight: 5, color: color.light }}>{membersCount.colab}</Text>
-                <Icon name='person' color='white' size={20}/>
+                <Icon name="person" color="white" size={20} />
               </View>
             </View>
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={() => onToggleFilter('viadukt')}>
-          <Image resizeMode='cover' style={styles.tile} source={require('../Styles/Assets/IHZ_150916_master_locations_1.jpg')} />
+          <Image resizeMode="cover" style={styles.tile} source={require('../Styles/Assets/IHZ_150916_master_locations_1.jpg')} />
           <View style={styles.wrap}>
-            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('viadukt')? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)'}]}>
-              <Icon name='location-on' size={40} color='white' />
+            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('viadukt') ? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)' }]}>
+              <Icon name="location-on" size={40} color="white" />
               <HeaderText style={{ color: color.light }}>Viadukt</HeaderText>
               <View style={{ flexDirection: 'row', paddingTop: 5 }}>
                 <Text style={{ paddingRight: 5, color: color.light }}>{membersCount.viadukt}</Text>
-                <Icon name='person' color='white' size={20}/>
+                <Icon name="person" color="white" size={20} />
               </View>
             </View>
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={() => onToggleFilter('garage')}>
-          <Image resizeMode='cover' style={styles.tile} source={require('../Styles/Assets/IHZ_150916_master_locations_L2.jpg')} />
+          <Image resizeMode="cover" style={styles.tile} source={require('../Styles/Assets/IHZ_150916_master_locations_L2.jpg')} />
           <View style={styles.wrap}>
-            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('garage')? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)'}]}>
-              <Icon name='location-on' size={40} color={ color.light } />
+            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('garage') ? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)' }]}>
+              <Icon name="location-on" size={40} color={ color.light } />
               <HeaderText style={{ color: color.light }}>Garage</HeaderText>
               <View style={{ flexDirection: 'row', paddingTop: 5 }}>
                 <Text style={{ paddingRight: 5, color: color.light }}>{membersCount.garage}</Text>
-                <Icon name='person' color='white' size={20}/>
+                <Icon name="person" color="white" size={20} />
               </View>
             </View>
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={() => onToggleFilter('new')}>
-          <Image resizeMode='cover' style={styles.tile} source={require('../Styles/Assets/new.jpg')} />
+          <Image resizeMode="cover" style={styles.tile} source={require('../Styles/Assets/new.jpg')} />
           <View style={styles.wrap}>
-            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('new')? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)'}]}>
-              <Icon name='today' size={40} color={ color.light } />
+            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('new') ? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)' }]}>
+              <Icon name="today" size={40} color={ color.light } />
               <HeaderText style={{ color: color.light }}>New Members</HeaderText>
               <View style={{ flexDirection: 'row', paddingTop: 5 }}>
                 <Text style={{ paddingRight: 5, color: color.light }}>{membersCount.newOnes}</Text>
-                <Icon name='person' color='white' size={20}/>
+                <Icon name="person" color="white" size={20} />
               </View>
             </View>
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={() => onToggleFilter('collaboration')}>
-          <Image resizeMode='cover' style={styles.tile} source={require('../Styles/Assets/collab.jpg')} />
+          <Image resizeMode="cover" style={styles.tile} source={require('../Styles/Assets/collab.jpg')} />
           <View style={styles.wrap}>
-            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('collaboration')? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)'}]}>
-              <Icon name='people' size={40} color={ color.light } />
+            <View style={ [styles.overlay, { backgroundColor: activeFilters.includes('collaboration') ? 'rgba(25,140,170,0.7)' : 'rgba(0,0,0,0.3)' }]}>
+              <Icon name="people" size={40} color={ color.light } />
               <HeaderText style={{ color: color.light }}>Open For Collaboration</HeaderText>
               <View style={{ flexDirection: 'row', paddingTop: 5 }}>
                 <Text style={{ paddingRight: 5, color: color.light }}>{membersCount.collaboration}</Text>
-                <Icon name='person' color='white' size={20}/>
+                <Icon name="person" color="white" size={20} />
               </View>
             </View>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.tile, { backgroundColor: color.red, width: width }]} onPress={() => resetAll()}>
+        <TouchableOpacity style={ [styles.tile, { backgroundColor: color.red, width }] } onPress={() => resetAll()}>
           <Icon name="public" size={40} color="white" />
           <HeaderText style={{ color: 'white', paddingTop: 3 }}>All Members</HeaderText>
           <View style={{ flexDirection: 'row', paddingTop: 5 }}>
             <Text style={{ paddingRight: 5, color: color.light }}>{membersCount.all}</Text>
-            <Icon name='person' color='white' size={20}/>
+            <Icon name="person" color="white" size={20} />
           </View>
         </TouchableOpacity>
       </View>
@@ -97,13 +96,17 @@ export const MemberListFilter = ({ onToggleFilter, activeFilters, membersCount, 
 };
 
 MemberListFilter.propTypes = { // eslint-disable-line immutable/no-mutation
+  onToggleFilter: PropTypes.func,
+  activeFilters: PropTypes.array,
+  membersCount: PropTypes.object,
+  resetAll: PropTypes.func
 };
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height - NavigationExperimental.Header.HEIGHT;
 const styles = StyleSheet.create({
   tile: {
-    height: height/4,
+    height: height / 4,
     width: width / 2,
     alignItems: 'center',
     justifyContent: 'center'
