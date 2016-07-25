292bb8c0fa683eb0eec0ebe9129decf746720844
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-07-25 14:42:26 +0200

    Added Tag Cloud example in playground

--- HubApp/App/Playground.js
+++ HubApp/App/Playground.js
@@ -1,62 +1,174 @@
 /* @flow weak */
 /* eslint-disable no-alert, no-console, max-len */
 import React from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Text,
   View,
+  TouchableOpacity,
   ScrollView,
   StyleSheet,
-  Dimensions
 } from 'react-native';
 
 import { color } from './Styles/color';
+import Immutable from 'immutable';
 
 export const Playground = () => {
+  const skillList = countList(members).sortBy(o => o.count).takeLast(50).sortBy(o => o.skill.name);
+  const min = Math.log(skillList.minBy(o => o.count).count);
+  const max = Math.log(skillList.maxBy(o => o.count).count);
   return (
     <ScrollView>
-      <View style={{ flexDirection: 'row', flexWrap: 'wrap', flex: 1 }}>
-        <View style={[styles.tile, { backgroundColor: color.blue }]}>
-          <Icon name="public" size={30} color="white" />
-          <Text style={{ color: 'white', paddingTop: 3 }}>All Members</Text>
-        </View>
-        <View style={[styles.tile, { backgroundColor: color.red }]}>
-          <Icon name="map" size={30} color="white" />
-          <Text style={{ color: 'white', paddingTop: 3 }}>Currently Here</Text>
-        </View>
-        <View style={[styles.tile, { backgroundColor: color.green }]}>
-          <Icon name="today" size={30} color="white" />
-          <Text style={{ color: 'white', paddingTop: 3 }}>New Members</Text>
-        </View>
-        <View style={[styles.tile, { backgroundColor: color.orange }]}>
-          <Icon name="home" size={30} color="white" />
-          <Text style={{ color: 'white', paddingTop: 3 }}>Staff</Text>
-        </View>
-        <View style={[styles.row, { backgroundColor: color.blue }]}>
-          <Icon name="email" size={30} color="white" />
-          <Text style={{ color: 'white', paddingTop: 3 }}>Email Your Host</Text>
-        </View>
-        <View style={[styles.row, { backgroundColor: color.green }]}>
-          <Icon name="event" size={30} color="white" />
-          <Text style={{ color: 'white', paddingTop: 3 }}>Events</Text>
-        </View>
-      </View>
-    </ScrollView>
+      <TagCloud skillList={skillList} min={min} max={max} addSkill={(skill) => alert(skill.count)} />
+   </ScrollView>
   );
 };
 
-const width = Dimensions.get('window').width;
+export const TagCloud = ({ skillList, min, max, addSkill, style }) => (
+  <View style={styles.skills}>
+    {
+      skillList.map(skill => (
+        <TouchableOpacity key={skill.skill.id} style={ [styles.tagButton, style] } onPress={() => addSkill(skill)}>
+          {
+            renderTag(skill, max, min, 12, 20)
+          }
+        </TouchableOpacity>
+      ))
+    }
+  </View>
+);
+
+TagCloud.propTypes = { // eslint-disable-line immutable/no-mutation
+  style: React.PropTypes.object,
+  addSkill: React.PropTypes.func,
+  skillList: React.PropTypes.object.isRequired,
+  min: React.PropTypes.number.isRequired,
+  max: React.PropTypes.number.isRequired
+};
+
+const renderTag = (skill, max, min, base, range) => {
+  const fontSize = Math.round((Math.log(skill.count) - min) * (range / (max - min)) + base);
+  const height = fontSize + fontSize / 5;
+  const borderRadius = height / 2;
+  const padding = height / 2;
+  return (
+    <Text style={ [styles.tag, { fontSize, height, borderRadius, paddingLeft: padding, paddingRight: padding }] }>
+      {skill.skill.name}
+    </Text>
+  );
+};
+
+const countList = members => {
+  const immutableMembers = Immutable.List(members);
+  return immutableMembers
+  .map(m => Immutable.List(m.skills))
+  .flatten()
+  .groupBy(s => s.id)
+  .map(l => ({ skill: l.first(), count: l.count() }));
+};
+
 const styles = StyleSheet.create({
-  tile: {
-    height: width / 2,
-    width: width / 2,
-    alignItems: 'center',
+  skills: {
+    paddingTop: 50,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  tagButton: {
+    margin: 3,
     justifyContent: 'center'
   },
-  row: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: width / 4,
-    width,
-  },
+  tag: {
+    backgroundColor: color.green,
+    color: color.light
+  }
 });
+
+const mem1 = {
+  name:  'livio',
+  skills:  [
+    {
+      id:  1,
+      name:  'javascript'
+    },
+    {
+      id: 2,
+      name:  'java'
+    },
+    {
+      id: 3,
+      name:  'jassen'
+    }
+  ]
+};
+
+const mem2 = {
+  name:  'yves',
+  skills: [
+    {
+      id: 2,
+      name:  'java'
+    },
+    {
+      id: 4,
+      name: 'css'
+    }
+  ]
+};
+
+const mem3 = {
+  name: 'marius',
+  skills:  [
+    {
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },{
+      id: 1,
+      name: 'javascript'
+    },
+    {
+      id: 5,
+      name: 'c#'
+    },
+    {
+      id: 6,
+      name: 'react'
+    }
+  ]
+};
+
+const members = [mem1, mem2, mem3];
